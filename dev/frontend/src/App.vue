456ea434cd23e0ac5a1346a2c265779addcbb405
breeder <breeder@sflswall.com.cn>
2026-08-17 22:46:12 +0800

    全局增加环境标识：右上角固定显示开发/生产环境徽标

--- dev/frontend/src/App.vue
+++ dev/frontend/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <router-view />
+  <EnvBadge />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import EnvBadge from './components/EnvBadge.vue';
+</script>
